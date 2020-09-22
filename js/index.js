2b0cd4cd800970384cc0438240a83a20a31e0f5b
SHLOKBAWA <shlokbawa007@hotmail.com>
2020-09-22 18:46:57 -0400

    navigation bar

--- js/index.js
+++ js/index.js
@@ -34,10 +34,10 @@ class Article extends Page{
         }
     }
 }
-
+const sName = "Shloksterssss Bawa";
 class Footer extends Page{
     render(){
-        const sName = "Shloksterssss Bawa";
+
         const yToday = new Date().getFullYear();
         $("footer").html(
             `&copy; ${yToday} ${sName}`
@@ -57,33 +57,16 @@ class Nav extends Page{
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#home">Project name</a>
+            <a class="navbar-brand" href="#home">Portfolio of ${sName}</a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#home">Home</a>
-                </li>
+                
                 <li><a href="#about">About</a>
                 </li>
                 <li><a href="#contact">Contact</a>
                 </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a>
-                        </li>
-                        <li><a href="#">Another action</a>
-                        </li>
-                        <li><a href="#">Something else here</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a>
-                        </li>
-                        <li><a href="#">One more separated link</a>
-                        </li>
-                    </ul>
-                </li>
+                
             </ul>
         </div>
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default [
-{"title":"index", "fname":"index.md"},
 {"title":"about", "fname":"about.md"},
+{"title":"index", "fname":"index.md"},
 {"title":"contact", "fname":"contact.md"}
 ];
